68cfc388aa3e416b40fe05d6dd8c4a78c8d7a447
lolapons <90765857+lolapons@users.noreply.github.com>
2025-05-30 14:08:41 +0200

    de momento bien! commit por si acaso, he puesto el scroll-bar pero me falta ajustarla a la derecha

--- src/app/components/wizards/wizard-crear-agente/wizard-crear-agente.module.ts
+++ src/app/components/wizards/wizard-crear-agente/wizard-crear-agente.module.ts
@@ -6,10 +6,11 @@ import { SearchModule } from '../../shared/search/search/search.module';
 import { LlmModelSelectCardModule } from '../../shared/llm-model-select-card/llm-model-select-card.module';
 import { ProgressIndicatorModule } from '../progress-indicator/progress-indicator.module';
 import { SelectInputModule } from '../../shared/select-input/select-input/select-input.module';
+import { ButtonModule } from '../../shared/button/button/button.module';
 
 @NgModule({
   declarations: [WizardCrearAgenteComponent],
-  imports: [CommonModule, FormsModule, SearchModule, LlmModelSelectCardModule, SelectInputModule, ProgressIndicatorModule],
+  imports: [CommonModule, FormsModule, SearchModule, LlmModelSelectCardModule, SelectInputModule, ProgressIndicatorModule, ButtonModule],
   exports: [WizardCrearAgenteComponent]
 })
 export class WizardCrearAgenteModule {} 
